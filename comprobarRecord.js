26bb49e952bbbbb46555960e09d47a9792e7596c
Fifita42 <102197218+Fifita42@users.noreply.github.com>
2022-09-15 14:23:59 -0300

    Update comprobarRecord.js

--- comprobarRecord.js
+++ comprobarRecord.js
@@ -2,14 +2,11 @@ let records;
 let aQuien;
 function comprobar(tiem)
 {
-    console.log('Entro a la funcion comprobar');
     let form = document.createElement('form');
     form.method = 'POST';
-    form.innerHTML = `
-    <input name="tiempo" value="${tiem}">
-    `;
+    form.innerHTML = `<input name="tiempo" value="${tiem}">`;
     const enviar = new FormData(form);
-    fetch('https://tablaemoji.000webhostapp.com/datos.php',{//enviar datos al php
+    fetch('https://tablaemoji.000webhostapp.com/datos.php',{
         method: 'POST',
         body: enviar
     })
@@ -29,7 +26,6 @@ function comprobar(tiem)
         }while(i<10);
         if(gano==1)
         {
-    console.log('Gano');
             actualizar(tiem);
         }
         });
@@ -37,50 +33,57 @@ function comprobar(tiem)
 
 function actualizar(tiem)
 {
-        swal("Ha superado un record!!, User name:", {
-            content: "input",
-            })
-            .then((value) => {
-            if(value!=''){
-             declararNombre(value);
-            }else swal("Error","Fallo en el registro","error");
-                });
-                
-       
-            function declararNombre(nom)
+        swal("Ha superado un record!!, User name:",
+        {
+        content: "input",
+        })
+        .then((value) => 
+        {
+            if(value!='')
             {
-                records.splice(aQuien,0,[aQuien+1,nombre.value,record.value]);
-                records.pop();
-                let i = 0;
-                records.forEach(element => {
-                    if(i>=aQuien)
-                    {
-                        element[0] = i+1;
-                    }
-                    i++;
-                });
-                
-                records.forEach(esto=>{
-                    let form = document.createElement('form');
-                    form.method = 'POST';
-                    let aEnvair = JSON.stringify(esto);//document.body.append(form);
-                    form.innerHTML = `<input name="data" value="${esto}">`;
-                    const enviar = new FormData(form);
-                    fetch('https://tablaemoji.000webhostapp.com/cambiarData.php',{//enviar datos al php
-                        method: 'POST',
-                        body: enviar
-                    })
-                    .then(res=>res.json())//recise la respuesta en json
-                    .then(dat =>
-                        {
-                            if(dat){
-                                swal("Okey!","Datos subidos exitosamente.",'success');
-                            }else{
-                                swal("Error!","Operación cancelada. :(",'success');
-                             }
-
-                        })
-                })
-            } 
+                declararNombre(value);
+            }
+            else swal("Error","Fallo en el registro","error");
+        });
             
+
+        function declararNombre(nom)
+        {
+            records.splice(aQuien,0,[aQuien+1,nombre.value,record.value]);
+            records.pop();
+            let i = 0;
+            records.forEach(element => 
+            {
+                if(i>=aQuien)
+                {
+                    element[0] = i+1;
+                }
+                i++;
+            });
+                
+            records.forEach(esto=>
+            {
+            let form = document.createElement('form');
+            form.method = 'POST';
+            let aEnvair = JSON.stringify(esto);//document.body.append(form);
+            form.innerHTML = `<input name="data" value="${esto}">`;
+            const enviar = new FormData(form);
+            fetch('https://tablaemoji.000webhostapp.com/cambiarData.php',
+            {//enviar datos al php
+                method: 'POST',
+                body: enviar
+            })
+            .then(res=>res.json())//recise la respuesta en json
+            .then(dat =>
+            {
+                if(dat)
+                {
+                    swal("Okey!","Datos subidos exitosamente.",'success');
+                }else
+                {
+                    swal("Error!","Operación cancelada. :(",'success');
+                }
+            })
+            })
+        }     
 }
